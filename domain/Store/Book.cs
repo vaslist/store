7b984ae83933595ecd06911f2cad815bf5c0bc5a
vaslist <kvasgms@gmail.com>
2023-08-07 16:16:41 +0300

    Добавить интерфейс репозитория книг

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -2,5 +2,13 @@
 
 public class Book
 {
+    public int Id { get; }
+    public string Title { get; }
 
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+     
 }
