c4cfa52ab193e17d9fc9370970a47fa2ccc181e6
vaslist <kvasgms@gmail.com>
2023-08-07 20:08:35 +0300

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepositry;
+        public SearchController(IBookRepository bookRepositry)
+        {
+            this.bookRepositry = bookRepositry;
+        }
+        public IActionResult Index(string query)
+        {
+            var books = bookRepositry.GetAllByTitle(query);
+            return View(books);
+        }
+    }
+}
