dd0e98f528faf4dbf6e164645f4a2159f8ff65e6
vaslist <kvasgms@gmail.com>
2023-08-20 18:04:13 +0300

    сливаем в один комит  "Реализовать репозитории"
    This reverts commit ef49662478f66d70117926db169ff3f094e8ddc9.
    
    squash! Реализовать репозитории
    
    Реализовать репозитории
    
    (cherry picked from commit f1c681f23a1a4b5321407db6b4e185b7808b4736)
    
    Добавить полнотекстовый поиск
    
    (cherry picked from commit 82709fe87654f0287a12874a5fc211a5e55b16e7)
    
    Включить миграции
    
    (cherry picked from commit 97c3bb0367a808c5dc7337e140810eb5b0b57d88)
    
    Создать классы DTO
    
    Создать классы DTO
    
    (cherry picked from commit 5ed9842b80fddd563f70904f6cf93854b697cc01)

--- domain/Store.Tests/OrderItemTests.cs
+++ domain/Store.Tests/OrderItemTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Store.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,51 +10,45 @@ namespace Store.Tests
     public class OrderItemTests
     {
         [Fact]
-        public void OrderItem_WithZeroCount_ThrowsArgumentOutOfRangeException()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
-            {
-                int count = 0;
-                new OrderItem(1, 1m, count);
-            });
-        }
-
-        [Fact]
         public void OrderItem_WithNegativCount_ThrowsArgumentOutOfRangeException()
         {
             Assert.Throws<ArgumentOutOfRangeException>(() =>
             {
                 int count = -1;
-                new OrderItem(1, 1m, count);
+                OrderItem.DtoFactory.Create(new OrderDto(), 1, 10m, count);
             });
         }
         [Fact]
         public void OrderItem_WithPositivCount_SetssCount()
         {
-            var orderItem = new OrderItem(1, 3m, 2);
+            var orderItem = OrderItem.DtoFactory.Create(new OrderDto(), 1, 10m, 2);
             Assert.Equal(1, orderItem.BookId);
+            Assert.Equal(10m, orderItem.Price);
             Assert.Equal(2, orderItem.Count);
-            Assert.Equal(3m, orderItem.Price);
         }
 
         [Fact]
         public void Count_WithNegativValue_ThrowsArgumentOutOfRangeException()
         {
-            var orderItem = new OrderItem(1, 5m, 2);
+            var orderItemDto = OrderItem.DtoFactory.Create(new OrderDto(), 1, 10m, 30);
+            var orderItem = OrderItem.Mapper.Map(orderItemDto);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => orderItem.Count = -1);
         }
         [Fact]
         public void Count_WithZeroValue_ThrowsArgumentOutOfRangeException()
         {
-            var orderItem = new OrderItem(1, 5m, 2);
+            var orderItemDto = OrderItem.DtoFactory.Create(new OrderDto(), 1, 10m, 30);
+            var orderItem = OrderItem.Mapper.Map(orderItemDto);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => orderItem.Count = 0);
         }
         [Fact]
         public void Count_WithPositivValue_SetsValue()
         {
-            var orderItem = new OrderItem(1, 5m, 2);
+            var orderItemDto = OrderItem.DtoFactory.Create(new OrderDto(), 1, 10m, 30);
+            var orderItem = OrderItem.Mapper.Map(orderItemDto);
+
             orderItem.Count = 10;
             Assert.Equal(10, orderItem.Count);
         }
---
+++ infrastructure/Store.Data.EF/Migrations/20230820171716_initialcreate.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Store.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Isbn = table.Column<string>(type: "nvarchar(17)", maxLength: 17, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CellPhone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    DeliveryUniqueCode = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    DeliveryDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeliveryPrice = table.Column<decimal>(type: "money", nullable: false),
+                    DeliveryParameters = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentServiceName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    PaymentDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentParameters = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "Isbn", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "D. Knuth", "This volume begins with basic programming concepts and techniques, then focuses more particularly on information structures-the representation of information inside a computer, the structural relationships between data elements and how to deal with them efficiently.", "ISBN0201038013", 7.19m, "Art Of Programming, Vol. 1" },
+                    { 2, "M. Fowler", "As the application of object technology--particularly the Java programming language--has become commonplace, a new problem has emerged to confront the software development community.", "ISBN0201485672", 12.45m, "Refactoring" },
+                    { 3, "B. W. Kernighan, D. M. Ritchie", "Known as the bible of C, this classic bestseller introduces the C programming language and illustrates algorithms, data structures, and programming techniques.", "ISBN0131101633", 14.98m, "C Programming Language" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
